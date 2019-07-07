5e9050b23c88cb0935d14a6b7ea688b862a83bb3
mboss <37351874+Modexus@users.noreply.github.com>
2019-07-07 21:57:41 +0200

    attempt number 154

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,30 @@
 </script>
 
 <style>
-	:global(body) {
-		background: white;
+	:global(body, html) {
+		height: 100%;
+		padding: 0;
+    	margin: 0;
+		overflow: scroll;
 	}
+
+	#timeline_bar {
+        content: '';
+        width: 0;
+        height: 100%;
+        position: absolute;
+        border: 1px solid black;
+        top: 0;
+        left: 100px;
+        z-index: 2;
+    }
+
+	div {
+		height: 45%;
+	}	
 </style>
 
 <Title/>
+<div/>
+<div id="timeline_bar"/>
 <Timeline/>
--- src/Title.svelte
+++ src/Title.svelte
@@ -7,18 +7,19 @@
 
 <style>
 	h1 {
-		font-size: 10rem;
+		font-size: 10em;
 		font-family: 'Fira Mono', monospace;
+        line-height: 70%;
         color: black;
         background: white;
         position: sticky;
-        display: flex;
         place-content: center;
         text-align: center;
+        margin: 25% 0 0 0;
         top: 0;
-        margin: 50vh 0 0 0;
-        z-index: 1;
+        z-index: 2;
 	}
 </style>
 
+<div/>
 <h1>Mike Boss</h1>
