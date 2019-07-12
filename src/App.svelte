aa40248dec50fddbaa721a07ca935e83c15cb85e
mboss <37351874+Modexus@users.noreply.github.com>
2019-07-12 12:33:01 +0200

    transition shorter and responsive layout

--- src/App.svelte
+++ src/App.svelte
@@ -6,11 +6,11 @@
 
 <div>
 	<div class="flex flex-row h-full w-full">
-		<div class="flex-none w-1/4"/>
+		<div class="flex-none w-1/6 md:w-1/5 lg:1/4"/>
 		<div class="flex-none flex flex-row w-4">
 			<Bar/>
 		</div>
-		<div class="flex-none w-3/4">
+		<div class="flex-none w-5/6 md:w-4/5 lg:3/6">
 			<Title/>
 			<Timeline/>
 		</div>
